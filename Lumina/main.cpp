5155f897bb176efe7805409389d2c80aeab455df
Daniel Bar <dabar@g.jct.ac.il>
2026-04-26 18:22:52 +0300

    feat: added logic window loop

--- Lumina/main.cpp
+++ Lumina/main.cpp
@@ -1,13 +1,39 @@
 #include <SFML/Graphics.hpp>
 
+void update(sf::RenderWindow& window);
+void draw(sf::RenderWindow& window);
+
+
 int main()
 {
+	sf::RenderWindow window(sf::VideoMode({ 800, 600 }), "RGP game");
 
-	while (true) {
-		sf::RenderWindow window(sf::VideoMode({ 800, 600 }), "RGP game");
+	while (window.isOpen()) {
+		
+		update(window);
+	    draw(window);
 
 	}
 
 
 	return 0;
+}
+
+void update(sf::RenderWindow& window) {
+
+	while (const std::optional event = window.pollEvent()) {
+
+		if (event->is<sf::Event::Closed>()) {
+			window.close();
+		}
+ 
+	}
+}
+
+void draw(sf::RenderWindow& window) {
+
+	window.clear(sf::Color::Red);
+
+	window.display();
+
 }
